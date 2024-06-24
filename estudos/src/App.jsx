9d2fb294bda3b50105b7d5bf6c107c47cbf789f0
“eugabrieldesousa” <eugabrieldesousa@gmail.com>
2024-06-24 11:39:43 -0300

    Meu primeiro component react

---
+++ estudos/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import FirstComponent from "./components/FirstComponent";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>Let's go</p>
+      <FirstComponent />
+    </>
+  );
+}
+
+export default App;
